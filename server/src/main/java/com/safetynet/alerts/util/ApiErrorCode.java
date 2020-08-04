a2ba80686b384d109a55b885b4edb83832bf5c46
np111 <oc.np111@protonmail.com>
2020-08-04 05:46:49 +0200

    Add rest ExceptionController

---
+++ server/src/main/java/com/safetynet/alerts/util/ApiErrorCode.java
@@ -0,0 +1,10 @@
+package com.safetynet.alerts.util;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class ApiErrorCode {
+    public static final String SERVER_EXCEPTION = "SERVER_EXCEPTION";
+    public static final String BAD_REQUEST = "BAD_REQUEST";
+    public static final String VALIDATION_FAILED = "VALIDATION_FAILED";
+}
