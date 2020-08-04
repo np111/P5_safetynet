890a1af0a1b040e0f79e5255e3be5b05a7bc325e
np111 <oc.np111@protonmail.com>
2020-08-04 04:29:19 +0200

    Add responses models

---
+++ api/src/main/java/com/safetynet/alerts/api/response/CommunityEmailResponse.java
@@ -0,0 +1,32 @@
+package com.safetynet.alerts.api.response;
+
+import com.safetynet.alerts.api.model.Person;
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.Singular;
+import lombok.SneakyThrows;
+
+/**
+ * List of {@linkplain Person#getEmail()} email addresses} of {@linkplain Person persons} living in a given {@linkplain
+ * Person#getCity()} city}.
+ */
+@lombok.Builder(builderClassName = "Builder")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CommunityEmailResponse implements Cloneable {
+    @Singular("email")
+    private List<String> emails;
+
+    @SneakyThrows
+    public CommunityEmailResponse clone() {
+        CommunityEmailResponse o = (CommunityEmailResponse) super.clone();
+        if (o.emails != null) {
+            o.emails = new ArrayList<>(emails);
+        }
+        return o;
+    }
+}
