d7e7aaeebf51e5055860acee671d6b8425d6f0fd
np111 <oc.np111@protonmail.com>
2020-10-15 03:44:48 +0200

    Add swagger-ui in /docs

--- dev.sh
+++ dev.sh
@@ -13,6 +13,7 @@ print_usage() {
   echo "${green} docker up -d${reset}       Create and start development containers (in background)"
   echo "${green} docker down${reset}        Stop and remove development containers"
   echo "${green} docker logs${reset}        Print development containers logs"
+  echo "${green} update-docs${reset}        Update docs"
 }
 
 docker_compose() {
@@ -21,6 +22,12 @@ docker_compose() {
     command='sudo'
   fi
   ${command} docker-compose -p 'P4_parking_system' -f '.dev/docker-compose.yml' "$@"
+}
+
+update_docs() {
+  curl 'http://localhost:8081/api-docs' \
+    | jq '.info.description = "<a href=\"https://github.com/np111/P5_safetynet\">View Source on GitHub</a>" |
+          .servers = []' >'./docs/api-docs.json'
 }
 
 # main
@@ -34,6 +41,9 @@ shift
 case "$command" in
 docker)
   docker_compose "$@"
+  ;;
+update-docs)
+  update_docs "$@"
   ;;
 *)
   echo "${red2}Error: '${command}' is not a dev command.${reset2}" >&2
